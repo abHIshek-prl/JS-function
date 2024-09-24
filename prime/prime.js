a9ef06ec2195fcb20c7bfce7088674584017f896
abhishek <abhishekparayil01@gmail.com>
2024-09-24 21:43:06 +0530

    prime number

---
+++ prime/prime.js
@@ -0,0 +1,13 @@
+function isPrime(num){
+    for(var i=0;i<=num+2;i++){
+       
+    }
+    if(num%num==0 && num%1==0){
+        console.log("is a prime number")
+    }
+    else{
+        console.log("is not a prime number")
+    }
+   
+}
+isPrime(2)
